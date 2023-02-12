b898d8fcda20635457b08dc285a778d91d5223c2
crappe91 <crappe1991@gamil.com>
2023-02-12 22:51:50 +0100

    ultimos cambios

--- js/personalizado.js
+++ js/personalizado.js
@@ -2,19 +2,19 @@
 function switchTheme() {
     var button = document.getElementById("switch");
     if (button.innerHTML === "Dark Theme") {
-      button.innerHTML = "Light Theme";
-      document.body.classList.add("oscuro");
-      button.classList.remove("btn-light");
-      button.classList.add("btn-dark");
-      button.classList.add("active");
+        button.innerHTML = "Light Theme";
+        document.body.classList.add("oscuro");
+        button.classList.remove("btn-light");
+        button.classList.add("btn-dark");
+        button.classList.add("active");
     } else {
-      button.innerHTML = "Dark Theme";
+        button.innerHTML = "Dark Theme";
 
-      document.body.classList.remove("oscuro");
-      button.classList.remove("btn-dark");
-      button.classList.add("btn-light");
-      button.classList.remove("active");
+        document.body.classList.remove("oscuro");
+        button.classList.remove("btn-dark");
+        button.classList.add("btn-light");
+        button.classList.remove("active");
     }
-  }
+}
 
-  document.getElementById("switch").addEventListener("click", switchTheme);
+document.getElementById("switch").addEventListener("click", switchTheme);
